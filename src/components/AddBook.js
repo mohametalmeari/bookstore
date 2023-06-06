7155052f32d811d95340f2d6838b83e7b1f6fc14
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-06 15:50:21 +0300

    Create README

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,11 +5,7 @@ const AddBook = () => (
     </h2>
     <form className="addbook-form">
       <input className="addbook-title" type="text" placeholder="Book title" />
-      <select className="addbook-list">
-        <option value="Action">Action</option>
-        <option value="Science Fiction" selected>Science Fiction</option>
-        <option value="Economy">Economy</option>
-      </select>
+      <input className="addbook-author" type="text" placeholder="Author" />
       <button className="addbook-btn" type="button">ADD BOOK</button>
     </form>
 
