6b80335117e4418c50dd5c9c128f2b575f6e41aa
mohametalmeari <mohamet.almeari@gmail.com>
2023-06-12 21:35:24 +0300

    Install Redux toolkit & Create Store and Slices

---
+++ src/redux/features/books/booksSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import uuid from 'react-uuid';
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, { payload }) => {
+      state.books.push({
+        id: uuid(),
+        title: payload.title,
+        author: payload.author,
+      });
+    },
+    removeBook: (state, { payload }) => {
+      const itemId = payload.id;
+      state.books = state.books.filter((item) => item.id !== itemId);
+    },
+  },
+
+});
+
+console.log(booksSlice);
+
+export const {
+  addBook, removeBook,
+} = booksSlice.actions;
+export default booksSlice.reducer;
